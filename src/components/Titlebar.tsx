26c5add3712dc645862f5a9ab6bddc29d21d782d
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-05-12 23:37:00 +0530

    feat: add Titlebar component

---
+++ src/components/Titlebar.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+
+import Link from "next/link";
+import AppLogo from "./AppLogo";
+import { Button } from "./ui/button";
+import { ThemeToggler } from "./ui/theme-toggler";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+export default function Titlebar() {
+  const titlebarItemColor = "text-zinc-700 dark:text-zinc-200";
+  const githubRepo = "https://github.com/ghoshRitesh12/odin";
+
+  return (
+    <header className="py-3 lg:px-0 border-b-[1px]">
+      <div
+        className={cn`
+          flex items-center justify-between 
+          mx-auto w-full max-w-[92%] 
+          md:max-w-[95%] xl:max-w-[85%] 2xl:max-w-[70%]
+        `}
+      >
+        <AppLogo iconColorStyle={titlebarItemColor} />
+
+        <div className="flex items-center gap-4">
+          <ThemeToggler iconColorStyle={titlebarItemColor} />
+
+          <Button
+            asChild
+            variant={"outline"}
+            className="w-10"
+            size={"icon"}
+          >
+            <Link
+              target="_blank"
+              href={githubRepo}
+              className={titlebarItemColor}
+            >
+              <GitHubLogoIcon
+                width={24}
+                height={24}
+                color="currentColor"
+              />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
