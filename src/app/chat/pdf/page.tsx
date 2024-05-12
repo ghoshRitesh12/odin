1e601f1d864e7cef38b3216dca842c1ad667d2b1
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-05-12 23:34:38 +0530

    feat: add pdf chat page

---
+++ src/app/chat/pdf/page.tsx
@@ -0,0 +1,11 @@
+import { ChatBreadcrumb } from "@/components/ChatBreadcrumb";
+
+export default function PdfChatPage() {
+  return (
+    <div>
+      <ChatBreadcrumb chatCategory="PDF" />
+
+      <div>pdf chat page</div>
+    </div>
+  );
+}
