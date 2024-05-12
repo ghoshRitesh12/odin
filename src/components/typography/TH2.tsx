32b76ccdb9e6373f1b817fc49d6b579e5b570615
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-05-12 23:36:50 +0530

    refactor: remove border and padding from TypographyH2 component

--- src/components/typography/TH2.tsx
+++ src/components/typography/TH2.tsx
@@ -5,7 +5,7 @@ export function TypographyH2({ children }: PropsWithChildren) {
   return (
     <h2
       className={cn`
-        scroll-m-20 border-b pb-2 text-3xl font-semibold 
+        scroll-m-20 text-3xl font-semibold 
         tracking-tight first:mt-0
       `}
     >
