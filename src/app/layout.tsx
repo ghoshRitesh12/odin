4863f5d9091a6157bd9deae178ea715e8d8678c3
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-05-12 23:34:05 +0530

    feat: update layout; add Titlebar and ThemeProvider components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import type { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { Source_Sans_3 as SourceSans } from "next/font/google";
 
+import Titlebar from "@/components/Titlebar";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const sourceSans = SourceSans({
   preload: true,
@@ -14,7 +16,7 @@ const sourceSans = SourceSans({
 });
 
 export const metadata: Metadata = {
-  title: "Odin Chatbot",
+  title: "Odin",
   description:
     "An AI chatbot named after the God of Wisdom who sacrificed an eye to gain immense knowledge",
 };
@@ -25,10 +27,30 @@ type RootLayoutProps = Readonly<{
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
       <body className={cn`__odin__ ${sourceSans.className}`}>
-        <main>{children}</main>
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Titlebar />
+
+          <main
+            className={cn`
+              pt-4 sm:pt-8 pb-8 mx-auto w-full max-w-[92%] 
+              md:max-w-[95%] xl:max-w-[85%] 2xl:max-w-[70%]
+            `}
+          >
+            {children}
+          </main>
+
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
